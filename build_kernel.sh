2eb9aefbec46a5951e6a7ad0dbda90fc985c8a48
Thiago Mattos <thiagohrm@gmail.com>
2023-02-09 13:17:39 -0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd ..
-cd kernel/arpi
-ARCH=arm64 scripts/kconfig/merge_config.sh arch/arm64/configs/bcm2711_defconfig kernel/configs/android-base.config kernel/configs/android-recommended.config
-ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make Image.gz
-ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- DTC_FLAGS="-@" make broadcom/bcm2711-rpi-4-b.dtb
-ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- DTC_FLAGS="-@" make overlays/vc4-kms-v3d-pi4.dtbo
+cd kernel/rpi
+ARCH=arm scripts/kconfig/merge_config.sh arch/arm/configs/bcm2709_defconfig kernel/configs/android-base.config kernel/configs/android-recommended.config
+ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make zImage
+ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make dtbs
